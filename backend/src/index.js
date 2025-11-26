523247c61e95d81bb80b9aba9b17379ea4261e51
Rk4609 <rakeshjangid8742@gmail.com>
2025-11-26 21:27:12 +0530

    Add middleware and utility classes for error handling and response formatting; include cookie-parser and cors dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,18 @@
-import dotenv from 'dotenv'
+import express from 'express'
+import dotenv from "dotenv";
 import connectDB from "../db/index.js";
 
+const app = express()
 dotenv.config({
-    path:'./env'
-})
-
-
-
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection failed !!!", err);
+  });
